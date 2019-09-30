c3a1574224f9dc82f1991298777727bc47aab09c
mramanjulu94 <53407664+mramanjulu94@users.noreply.github.com>
2019-09-30 10:43:14 -0400

    project 1 completed

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+alert('Hello world');
+
+var favoriteFood = 'Cheesesteak';
+document.write(favoriteFood);
+
+
+
+/* var myName = 'Bob';
+document.write(myName);
+
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize); */
