849e6a535c4bee2f5469308256a2af2b6f2bb669
mramanjulu94 <53407664+mramanjulu94@users.noreply.github.com>
2019-10-14 21:16:42 -0400

    looping through the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 var repository = [
-  {name: "Bulbasaur",height: 7, type: ['grass', 'posion']},
-  {name: "Charmander", height: 11, type: ['fire']},
-  {name: "Squirtle", height: 9, type: ['water']}
+  {name: 'Bulbasaur', height: 7, type: ['grass', 'posion']},
+  {name: 'Charmander', height: 11, type: ['fire']},
+  {name: 'Squirtle', height: 9, type: ['water']}
 ];
+
+for(var i = 0; i < repository.length; i++)
+{
+  if(repository[i].height > 10)
+  {
+    document.write(repository[i].name + "  (height: " + repository[i].height + ")" + " type: " + repository[i].type + "   -  Wow so tall! " +  "<br>" +  "<br>" );
+
+  }
+  else
+document.write(repository[i].name + "  (height: " + repository[i].height + ")" + " type: " + repository[i].type + " " + "<br>" +  "<br>");
+}
